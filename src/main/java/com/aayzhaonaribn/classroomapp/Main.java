454444e4bcb248e7301416ab6fafaafa7c5bd8e7
Aaron Zhao <aaron.y.zhao1@gmail.com>
2024-01-26 15:11:37 -0500

    Refactor code, begin parsing implementation Attempting to parse txt using Scanner from java

--- src/main/java/com/aayzhaonaribn/classroomapp/Main.java
+++ src/main/java/com/aayzhaonaribn/classroomapp/Main.java
@@ -9,29 +9,60 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Main {
-    static final String inputPath = "spring2024.pdf";
-    static final String outputPath = "pdf.txt";
-    public static void main(String[] args) {
-        //System.out.println("Hello world");
+    static final String INPUT_PATH = "spring2024.pdf";
+    static final String OUTPUT_PATH = "pdf.txt";
+    static final boolean DEBUG_MODE = true;
 
+    public static void main(String[] args) {
+        ResourceLoader loader = new ResourceLoader(); // initialize resource loader
+        //System.out.println("Hello world"); // because why not
+
+        // Startup operations
+        // Load data from PDF
+        // generates a text file with all the pdf text contained within
+        loadPDFToText(loader);
+
+        // open a scanner for the newly made .txt file
+        File textFile = null;
+        try {
+            textFile = loader.getFileFromResource(OUTPUT_PATH);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            if (DEBUG_MODE) System.out.println("Error reading generated text file");
+        }
+        assert textFile != null;
+
+        Scanner scan = null;
+        try {
+            scan = new Scanner(textFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+            if (DEBUG_MODE) System.out.println("Text ver of pdf missing");
+        }
+
+
+
+    }
+
+    private static void loadPDFToText(ResourceLoader loader) {
         PDFToText pdfParser = new PDFToText();
-        ResourceLoader loader = new ResourceLoader();
         File inputFile = null;
         File outputFile = null;
         try {
-            outputFile = loader.getFileFromResource(outputPath);
+            outputFile = loader.getFileFromResource(OUTPUT_PATH);
         } catch (URISyntaxException e) {
             e.printStackTrace();
             System.out.println("URI syntax error");
         }
 
         try {
-            inputFile = loader.getFileFromResource(inputPath);
+            inputFile = loader.getFileFromResource(INPUT_PATH);
         } catch (URISyntaxException e) {
             e.printStackTrace();
-            System.out.println("URI syntax error");
+            if (DEBUG_MODE) System.out.println("URI syntax error");
         }
 
         assert outputFile != null;
@@ -44,7 +75,7 @@ public class Main {
             e.printStackTrace();
         }
 
-        System.out.println(result);
+        if (DEBUG_MODE) System.out.println(result); // several thousand lines of output to console
 
         try {
             Files.writeString(path, result, StandardCharsets.UTF_8);
@@ -52,7 +83,6 @@ public class Main {
             e.printStackTrace();
             System.out.println("Invalid path");
         }
-
     }
 
 }
