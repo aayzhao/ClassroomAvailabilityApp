8c84d85b5c46e5cfa0e82a5d5552cc7543d3f43d
Aaron Zhao <aaron.y.zhao1@gmail.com>
2024-01-26 14:00:08 -0500

    Implemented a parser class

--- src/main/java/com/aayzhaonaribn/classroomapp/Main.java
+++ src/main/java/com/aayzhaonaribn/classroomapp/Main.java
@@ -1,8 +1,24 @@
 package com.aayzhaonaribn.classroomapp;
+import com.aayzhaonaribn.parsing.Parser;
+
+import java.io.IOException;
 
 public class Main {
+    static final String filePath = "spring2024.pdf";
     public static void main(String[] args) {
         System.out.println("Hello world");
+        Parser parser = new Parser();
+        String result = "";
+
+        try {
+            result = parser.parse(filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(result);
+
     }
+
 }
 
---
+++ src/main/java/com/aayzhaonaribn/parsing/Parser.java
@@ -0,0 +1,49 @@
+package com.aayzhaonaribn.parsing;
+import org.apache.pdfbox.*;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class Parser {
+    public String parse(String fileName) throws IOException {
+        Parser parser = new Parser();
+        File pdfFile = null;
+
+        try {
+            pdfFile = parser.getFileFromResource(fileName);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        PDFTextStripper text = new PDFTextStripper();
+        text.setSortByPosition(true);
+        PDDocument doc = null;
+
+        try {
+            assert pdfFile != null;
+            doc = Loader.loadPDF(pdfFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return text.getText(doc);
+    }
+
+    private File getFileFromResource(String fileName) throws URISyntaxException {
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource(fileName);
+        if (resource == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        } else {
+
+            // failed if files have whitespaces or special characters
+            //return new File(resource.getFile());
+
+            return new File(resource.toURI());
+        }
+
+    }}
