a138fcfa116db19a0338dd4e1f27d6261750dbad
Aaron Zhao <aaron.y.zhao1@gmail.com>
2024-01-26 14:45:38 -0500

    Refactored resource loading process Now use ResourceLoader class to load resources/get files

--- src/main/java/com/aayzhaonaribn/classroomapp/Main.java
+++ src/main/java/com/aayzhaonaribn/classroomapp/Main.java
@@ -1,5 +1,6 @@
 package com.aayzhaonaribn.classroomapp;
-import com.aayzhaonaribn.parsing.Parser;
+import com.aayzhaonaribn.parsing.PDFToText;
+import com.aayzhaonaribn.parsing.ResourceLoader;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,24 +11,35 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
-    static final String filePath = "spring2024.pdf";
+    static final String inputPath = "spring2024.pdf";
     static final String outputPath = "pdf.txt";
     public static void main(String[] args) {
         //System.out.println("Hello world");
 
-        Parser parser = new Parser();
+        PDFToText pdfParser = new PDFToText();
+        ResourceLoader loader = new ResourceLoader();
+        File inputFile = null;
         File outputFile = null;
         try {
-            outputFile = parser.getFileFromResource(outputPath);
+            outputFile = loader.getFileFromResource(outputPath);
         } catch (URISyntaxException e) {
             e.printStackTrace();
             System.out.println("URI syntax error");
         }
+
+        try {
+            inputFile = loader.getFileFromResource(inputPath);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            System.out.println("URI syntax error");
+        }
+
+        assert outputFile != null;
         Path path = Paths.get(outputFile.getPath());
         String result = "";
 
         try {
-            result = parser.parse(filePath);
+            result = pdfParser.parse(inputFile);
         } catch (IOException e) {
             e.printStackTrace();
         }
---
+++ src/main/java/com/aayzhaonaribn/parsing/ResourceLoader.java
@@ -0,0 +1,23 @@
+package com.aayzhaonaribn.parsing;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class ResourceLoader {
+    public File getFileFromResource(String fileName) throws URISyntaxException {
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource(fileName);
+        if (resource == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        } else {
+
+            // failed if files have whitespaces or special characters
+            //return new File(resource.getFile());
+
+            return new File(resource.toURI());
+        }
+
+    }
+}
