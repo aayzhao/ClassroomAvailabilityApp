43d5b9e501f2965bc9ea061b2b6129ff9d80684a
Aaron Zhao <aaron.y.zhao1@gmail.com>
2024-01-27 15:53:54 -0500

    Added method overrides for comparisons for Course.java class

--- src/main/java/classes/Course.java
+++ src/main/java/classes/Course.java
@@ -3,10 +3,26 @@ package classes;
 import java.util.*;
 
 public class Course {
-    private String courseCode;
+    private String courseCode; // String containing the course major code, class code, and section concatenated
     private String building;
     private String room;
+    private int courseNumber; // unique course identifier number
     private List<TimeSlot> timeSlots;
+
+    /**
+     * Constructor that does not require a timeslot list, and instead a course identifier number
+     * @param _courseCode
+     * @param _building
+     * @param _room
+     * @param _courseNumber Unique identifier for the course
+     */
+    public Course(String _courseCode, String _building, String _room, int _courseNumber) {
+        this.courseCode = _courseCode;
+        this.building = _building;
+        this.room = _room;
+        this.timeSlots = new ArrayList<>();
+        this.courseNumber = _courseNumber;
+    }
 
     public Course(String _courseCode, String _building, String _room, List<TimeSlot> _timeSlots) {
         this.courseCode = _courseCode;
@@ -29,5 +45,16 @@ public class Course {
 
     public List<TimeSlot> getTimeSlot(){
         return timeSlots;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) throw new IllegalArgumentException("Object cannot be null");
+        return this.toString().equals(other.toString());
+    }
+
+    @Override
+    public String toString(){
+        return Integer.toString(this.courseNumber);
     }
 }
